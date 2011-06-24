ec0099c5d83de4a5861d016990e6e1c17ede187e
Nick Dunn <nick@nick-dunn.co.uk>
2011-06-24 15:53:06 +0100

    Updated docs for 1.1.3

--- README.markdown
+++ README.markdown
@@ -1,7 +1,7 @@
 # Upload Select Box Field
 
-* Version: 1.1.2
-* Build Date: 2011-02-07
+* Version: 1.1.3
+* Build Date: 2011-06-24
 * Author: Nick Dunn, Brendan Abbott
 * Requirements: Symphony 2.0.6+
 
--- extension.driver.php
+++ extension.driver.php
@@ -4,8 +4,8 @@
 
 		public function about(){
 			return array('name' => 'Field: Uploaded File Select Box',
-						 'version' => '1.1.2',
-						 'release-date' => '2011-02-07',
+						 'version' => '1.1.3',
+						 'release-date' => '2011-06-24',
 						 'author' => array('name' => 'Nick Dunn, Brendan Abbott',
 										   'website' => 'http://nick-dunn.co.uk')
 				 		);
